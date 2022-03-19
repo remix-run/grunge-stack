197188e18315d46780a4f3f6b3b74a06055ace9f
joe matune <44626877+joematune@users.noreply.github.com>
2022-03-19 15:17:04 +0200

    feat(models): add User and Note type references (#22)
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- app/session.server.ts
+++ app/session.server.ts
@@ -25,7 +25,9 @@ export async function getSession(request: Request) {
   return sessionStorage.getSession(cookie);
 }
 
-export async function getUserId(request: Request): Promise<string | undefined> {
+export async function getUserId(
+  request: Request
+): Promise<User["id"] | undefined> {
   const session = await getSession(request);
   const userId = session.get(USER_SESSION_KEY);
   return userId;
@@ -44,7 +46,7 @@ export async function getUser(request: Request): Promise<null | User> {
 export async function requireUserId(
   request: Request,
   redirectTo: string = new URL(request.url).pathname
-): Promise<string> {
+): Promise<User["id"]> {
   const userId = await getUserId(request);
   if (!userId) {
     const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
