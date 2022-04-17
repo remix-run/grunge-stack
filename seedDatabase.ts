b5c5b28085341821641c926a4f4a0f15e1ac6864
Shannon Oram <shannon.oram@gmail.com>
2022-04-17 12:42:53 +1000

    add comment

--- seedDatabase.ts
+++ seedDatabase.ts
@@ -1,3 +1,7 @@
+// This script will seed the database in your local development environment.
+// It doesn't run by default. The script needs to be enabled. See the
+// prefs.arc.example file for more details.
+
 import { createUser } from "./app/models/user.server";
 
 (async () => {
