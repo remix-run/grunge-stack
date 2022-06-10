5efce152bc5f111c2b105d070018f29eb2f68f36
Logan McAnsh <logan@mcan.sh>
2022-06-10 14:30:46 -0400

    Update cypress.config.ts
    Co-authored-by: Mehdi Achour <machour@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      const isDev = config.watchForFileChanges;
       const port = process.env.PORT ?? "3000";
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
         baseUrl: `http://localhost:${port}`,
