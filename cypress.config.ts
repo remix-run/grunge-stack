800ea3f5c4d483c858d7391872bc2b48ff6d5fa7
Logan McAnsh <logan@mcan.sh>
2022-06-13 16:32:55 -0400

    chore: remove dev.js (#70)
    * chore: remove dev.js
    
    closes #36
    
    * chore: update PORT to always be 3000
    
    * Update cypress.config.ts
    
    Co-authored-by: Mehdi Achour <machour@gmail.com>
    
    Co-authored-by: Mehdi Achour <machour@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      const isDev = config.watchForFileChanges;
-      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+      const port = process.env.PORT ?? "3000";
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
         baseUrl: `http://localhost:${port}`,
         video: !process.env.CI,
