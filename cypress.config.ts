ba40052d54e791ddb987ef65e7698b3cc10553f9
Logan McAnsh <logan@mcan.sh>
2022-06-22 14:06:07 -0400

    Revert "chore: remove dev.js" (#75)

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      const port = process.env.PORT ?? "3000";
+      const isDev = config.watchForFileChanges;
+      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
         baseUrl: `http://localhost:${port}`,
         video: !process.env.CI,
