912b7f3b762fccf4f976746146cb5151ece04341
RichardHpa <richard.m.hpa@gmail.com>
2022-07-27 11:51:23 +1200

    added a checkUrl function to make sure that the DOM has been mounted

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -14,6 +14,17 @@ declare global {
        *    cy.login({ email: 'whatever@example.com' })
        */
       login: typeof login;
+      /**
+       * Extends the standard visit command to wait for the page to load
+       *
+       * @returns {typeof visitAndCheck}
+       * @memberof Chainable
+       * @example
+       *    cy.visitAndCheck('/')
+       *  @example
+       *    cy.visitAndCheck('/', 500)
+       */
+      visitAndCheck: typeof visitAndCheck;
     }
   }
 }
@@ -28,4 +39,15 @@ function login({
   return cy.get("@user");
 }
 
+// We're waiting a second because of this issue happen randomly
+// https://github.com/cypress-io/cypress/issues/7306
+// Also added custom types to avoid getting detached
+// https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
+// ===========================================================
+function visitAndCheck(url: string, waitTime: number = 1000) {
+  cy.visit(url);
+  cy.location("pathname").should("contain", url).wait(waitTime);
+}
+
 Cypress.Commands.add("login", login);
+Cypress.Commands.add("visitAndCheck", visitAndCheck);
