0f3d284acff12587f2929e3dc4973e5d25abb72b
Michaël De Boey <info@michaeldeboey.be>
2023-03-16 00:26:58 +0100

    feat: remove deprecated `serverBuildTarget` usage

--- remix.config.js
+++ remix.config.js
@@ -4,13 +4,11 @@ const path = require("path");
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
   cacheDirectory: "./node_modules/.cache/remix",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/_static/build/",
-  serverBuildTarget: "arc",
-  server: "./server.ts",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  publicPath: "/_static/build/",
+  server: "./server.ts",
+  serverBuildPath: "server/index.js",
   routes(defineRoutes) {
     return defineRoutes((route) => {
       if (process.env.NODE_ENV === "production") return;
