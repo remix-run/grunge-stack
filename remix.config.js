a6fae1ff723aeecf356fac2493d29ee421c457cf
Michaël De Boey <info@michaeldeboey.be>
2023-03-16 20:32:32 +0100

    feat: enable `v2_routeConvention` flag

--- remix.config.js
+++ remix.config.js
@@ -5,13 +5,14 @@ const path = require("path");
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  future: {
+    v2_meta: true,
+    v2_routeConvention: true,
+  },
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   publicPath: "/_static/build/",
   server: "./server.ts",
   serverBuildPath: "server/index.js",
-  future: {
-    v2_meta: true,
-  },
   routes(defineRoutes) {
     return defineRoutes((route) => {
       if (process.env.NODE_ENV === "production") return;
