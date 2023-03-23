ef15ae52be5d01a13a594c1b768699742a7f3ef1
Michaël De Boey <info@michaeldeboey.be>
2023-03-23 03:35:16 +0100

    feat: enable `v2_normalizeFormMethod` flag (#131)

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
@@ -11,6 +9,7 @@ module.exports = {
   serverBuildPath: "server/index.js",
   future: {
     v2_meta: true,
+    v2_normalizeFormMethod: true,
   },
   routes(defineRoutes) {
     return defineRoutes((route) => {
