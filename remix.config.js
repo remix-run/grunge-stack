5852d3a9e6532a33ad6ae276007f25c973a7afb9
Michaël De Boey <info@michaeldeboey.be>
2023-03-25 16:45:04 +0100

    feat: enable `v2_errorBoundary` flag (#129)

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
+    v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
