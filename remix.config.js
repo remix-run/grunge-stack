f69680709b34a013af306108e7a231e02bbf91a7
Michaël De Boey <info@michaeldeboey.be>
2023-09-15 21:10:19 +0200

    feat: upgrade to Remix v2 (#166)

--- remix.config.js
+++ remix.config.js
@@ -3,21 +3,11 @@ import path from "node:path";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   cacheDirectory: "./node_modules/.cache/remix",
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
   publicPath: "/_static/build/",
-  postcss: true,
   server: "server.ts",
   serverBuildPath: "server/index.mjs",
   serverModuleFormat: "esm",
-  tailwind: true,
   routes: (defineRoutes) =>
     defineRoutes((route) => {
       if (process.env.NODE_ENV === "production") return;
--- remix.init/index.js
+++ remix.init/index.js
@@ -1,39 +1,12 @@
-const { execSync } = require("child_process");
-const crypto = require("crypto");
-const fs = require("fs/promises");
-const path = require("path");
+const { execSync } = require("node:child_process");
+const crypto = require("node:crypto");
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const { toLogicalID } = require("@architect/utils");
 const PackageJson = require("@npmcli/package-json");
 const inquirer = require("inquirer");
 const semver = require("semver");
-const YAML = require("yaml");
-
-const cleanupDeployWorkflow = (deployWorkflow, deployWorkflowPath) => {
-  delete deployWorkflow.jobs.typecheck;
-  deployWorkflow.jobs.deploy.needs = deployWorkflow.jobs.deploy.needs.filter(
-    (need) => need !== "typecheck",
-  );
-
-  return [fs.writeFile(deployWorkflowPath, YAML.stringify(deployWorkflow))];
-};
-
-const cleanupRemixConfig = (remixConfig, remixConfigPath) => {
-  const newRemixConfig = remixConfig
-    .replace("server.ts", "server.js")
-    .replace("create-user.ts", "create-user.js");
-
-  return [fs.writeFile(remixConfigPath, newRemixConfig)];
-};
-
-const cleanupVitestConfig = (vitestConfig, vitestConfigPath) => {
-  const newVitestConfig = vitestConfig.replace(
-    "setup-test-env.ts",
-    "setup-test-env.js",
-  );
-
-  return [fs.writeFile(vitestConfigPath, newVitestConfig)];
-};
 
 const getPackageManagerCommand = (packageManager) =>
   // Inspired by https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L38-L103
@@ -70,15 +43,6 @@ const getPackageManagerVersion = (packageManager) =>
 
 const getRandomString = (length) => crypto.randomBytes(length).toString("hex");
 
-const readFileIfNotTypeScript = (
-  isTypeScript,
-  filePath,
-  parseFunction = (result) => result,
-) =>
-  isTypeScript
-    ? Promise.resolve()
-    : fs.readFile(filePath, "utf-8").then(parseFunction);
-
 const removeUnusedDependencies = (dependencies, unusedDependencies) =>
   Object.fromEntries(
     Object.entries(dependencies).filter(
@@ -86,14 +50,12 @@ const removeUnusedDependencies = (dependencies, unusedDependencies) =>
     ),
   );
 
-const updatePackageJson = ({ APP_NAME, isTypeScript, packageJson }) => {
+const updatePackageJson = ({ APP_NAME, packageJson }) => {
   const {
     devDependencies,
     scripts: {
       "format:repo": _repoFormatScript,
       "lint:repo": _repoLintScript,
-      typecheck,
-      validate,
       ...scripts
     },
   } = packageJson.content;
@@ -103,34 +65,17 @@ const updatePackageJson = ({ APP_NAME, isTypeScript, packageJson }) => {
     devDependencies: removeUnusedDependencies(
       devDependencies,
       // packages that are only used for linting the repo
-      ["eslint-plugin-markdown", "eslint-plugin-prefer-let"].concat(
-        isTypeScript ? [] : ["ts-node"],
-      ),
+      ["eslint-plugin-markdown", "eslint-plugin-prefer-let"],
     ),
-    scripts: isTypeScript
-      ? { ...scripts, typecheck, validate }
-      : { ...scripts, validate: validate.replace(" typecheck", "") },
+    scripts,
   });
 };
 
-const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
-  const FILE_EXTENSION = isTypeScript ? "ts" : "js";
-
+const main = async ({ packageManager, rootDirectory }) => {
   const APP_ARC_PATH = path.join(rootDirectory, "./app.arc");
   const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
   const ENV_PATH = path.join(rootDirectory, ".env");
   const README_PATH = path.join(rootDirectory, "README.md");
-  const DEPLOY_WORKFLOW_PATH = path.join(
-    rootDirectory,
-    ".github",
-    "workflows",
-    "deploy.yml",
-  );
-  const REMIX_CONFIG_PATH = path.join(rootDirectory, "remix.config.js");
-  const VITEST_CONFIG_PATH = path.join(
-    rootDirectory,
-    `vitest.config.${FILE_EXTENSION}`,
-  );
 
   const DIR_NAME = path.basename(rootDirectory);
   const SUFFIX = getRandomString(2);
@@ -139,23 +84,10 @@ const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
     // get rid of anything that's not allowed in an app name
     .replace(/[^a-zA-Z0-9-_]/g, "-");
 
-  const [
-    appArc,
-    env,
-    readme,
-    deployWorkflow,
-    remixConfig,
-    vitestConfig,
-    packageJson,
-  ] = await Promise.all([
+  const [appArc, env, readme, packageJson] = await Promise.all([
     fs.readFile(APP_ARC_PATH, "utf-8"),
     fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
     fs.readFile(README_PATH, "utf-8"),
-    readFileIfNotTypeScript(isTypeScript, DEPLOY_WORKFLOW_PATH, (s) =>
-      YAML.parse(s),
-    ),
-    readFileIfNotTypeScript(isTypeScript, REMIX_CONFIG_PATH),
-    readFileIfNotTypeScript(isTypeScript, VITEST_CONFIG_PATH),
     PackageJson.load(rootDirectory),
   ]);
 
@@ -164,7 +96,7 @@ const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
     `SESSION_SECRET="${getRandomString(16)}"`,
   );
 
-  updatePackageJson({ APP_NAME, isTypeScript, packageJson });
+  updatePackageJson({ APP_NAME, packageJson });
 
   const initInstructions = `
 - First run this stack's \`remix.init\` script and commit the changes it makes to your project.
@@ -181,7 +113,7 @@ const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
     .replace(new RegExp("RemixGrungeStack", "g"), toLogicalID(APP_NAME))
     .replace(initInstructions, "");
 
-  const fileOperationPromises = [
+  await Promise.all([
     fs.writeFile(
       APP_ARC_PATH,
       appArc.replace("grunge-stack-template", APP_NAME),
@@ -203,23 +135,7 @@ const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
     fs.rm(path.join(rootDirectory, ".github", "PULL_REQUEST_TEMPLATE.md")),
     fs.rm(path.join(rootDirectory, ".eslintrc.repo.cjs")),
     fs.rm(path.join(rootDirectory, "LICENSE.md")),
-  ];
-
-  if (!isTypeScript) {
-    fileOperationPromises.push(
-      ...cleanupDeployWorkflow(deployWorkflow, DEPLOY_WORKFLOW_PATH),
-    );
-
-    fileOperationPromises.push(
-      ...cleanupRemixConfig(remixConfig, REMIX_CONFIG_PATH),
-    );
-
-    fileOperationPromises.push(
-      ...cleanupVitestConfig(vitestConfig, VITEST_CONFIG_PATH),
-    );
-  }
-
-  await Promise.all(fileOperationPromises);
+  ]);
 
   await askSetupQuestions({ packageManager, rootDirectory }).catch((error) => {
     if (error.isTtyError) {
