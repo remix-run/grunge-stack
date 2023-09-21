c64605a1cfbd3bf7f74b54a9b983c687c47f5aac
Alex Mitchell <shamsup@users.noreply.github.com>
2023-09-21 15:15:01 -0700

    add `installGlobals` to server handler for node16.x

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
 import { createRequestHandler } from "@remix-run/architect";
+import { installGlobals } from "@remix-run/node";
 import * as build from "@remix-run/dev/server-build";
+
+installGlobals();
 
 if (process.env.NODE_ENV !== "production") {
   require("./mocks");
